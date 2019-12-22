b73869d4087f399a1405cde6207d85130acd4fb5
phuchealer91 <pepegvcn2@gmail.com>
2019-12-22 22:38:08 +0700

    Update điều kiện kiểm tra xem danh mục có sản phẩm chưa (có -> ko cho xóa, chưa -> cho xóa)

--- admin/modules/category/delete.php
+++ admin/modules/category/delete.php
@@ -1,12 +1,14 @@
 <?php
+require_once  __DIR__."/../../autoload/autoload.php";
 $id = intval(getInput('id'));
 $deleteCategory = $db->fetchID("category",$id);
 if(empty($deleteCategory)){
     $_SESSION['error'] = "Dữ liệu không tồn tại.";
     redirectAdmin("category");
 }
-    //Erorr rỗng thì không có lỗi
-    if(empty($error)){
+    $is_product = $db->fetchOne("product","category_id = '$id'");
+    //Kiểm tra xem danh mục có sản phẩm chưa
+    if($is_product == NULL){
         $id_delete = $db->delete("category",$id);
         if($id_delete > 0){
             $_SESSION['success'] = "Xóa thành công.";
@@ -17,6 +19,10 @@ if(empty($deleteCategory)){
             $_SESSION['error'] = "Xóa không thành công.";
             redirectAdmin("category");
         }
+    }
+    else {
+        $_SESSION['error'] = "Không thể xóa. Danh mục đã có sản phẩm.";
+        redirectAdmin("category");
     }
 
 ?>
